65a6afc61a251954170ca3d6920f3038d03c2987
Carl <cboettig@gmail.com>
2021-10-29 04:11:22 +0000

    RETROACTIVE BREAKING patch: determine abundance using taxonomy-corrected counts, not raw sorting table counts.

--- 02_targets.R
+++ 02_targets.R
@@ -16,25 +16,26 @@ print(neon_dir())
 
 ## assumes data have been downloaded and stored with:
 # neon_download("DP1.10022.001")
-#neon_store(product = "DP1.10022.001")
+# neon_store(product = "DP1.10022.001")
 
 
 ## Load data from raw files
-sorting <- neon_table("bet_sorting")
-para <- neon_table("bet_parataxonomistID")
-expert <- neon_table("bet_expertTaxonomistIDProcessed")
-field <- neon_table("bet_fielddata")
+sorting <- neon_table("bet_sorting-expanded")
+para <- neon_table("bet_parataxonomistID-expanded")
+expert <- neon_table("bet_expertTaxonomistIDProcessed-expanded")
+field <- neon_table("bet_fielddata-expanded")
 
 
 #### Generate derived richness table  ####################
-beetles <- resolve_taxonomy(sorting, para, expert) %>% 
+beetles <- resolve_taxonomy(sorting, para, expert) %>%
   mutate(iso_week = ISOweek::ISOweek(collectDate),
-         time = ISOweek::ISOweek2date(paste0(iso_week, "-1")))
+         time = ISOweek::ISOweek2date(paste0(iso_week, "-1"))) %>%
+  as_tibble()
 
-richness <- beetles %>%  
+richness <- beetles %>%
   select(taxonID, siteID, collectDate, time) %>%
   distinct() %>%
-  count(siteID, time) %>% 
+  count(siteID, time) %>%
   rename(richness = n)  %>%
   ungroup()
 
@@ -42,29 +43,31 @@ richness <- beetles %>%
 
 #### Generate derived abundance table ####################
 
-effort <- field %>% 
-  mutate(iso_week = ISOweek::ISOweek(collectDate),
-         time = ISOweek::ISOweek2date(paste0(iso_week, "-1"))) %>% 
-  group_by(siteID, time) %>% 
-  summarise(trapnights = as.integer(sum(collectDate - setDate)),
-            .groups = "drop")
+## Does not reflect taxonomic corrections!
+## Allows for some counts even when richness is NA
 
-counts <- sorting %>% 
+effort <- field %>%
   mutate(iso_week = ISOweek::ISOweek(collectDate),
          time = ISOweek::ISOweek2date(paste0(iso_week, "-1"))) %>%
   group_by(siteID, time) %>%
-  summarise(count = sum(as.numeric(individualCount), na.rm = TRUE), 
+  summarise(trapnights = as.integer(sum(collectDate - setDate)),
             .groups = "drop")
 
-abund <- counts %>% 
-  left_join(effort) %>% 
+counts <- beetles %>%
+  mutate(iso_week = ISOweek::ISOweek(collectDate),
+         time = ISOweek::ISOweek2date(paste0(iso_week, "-1"))) %>%
+  group_by(siteID, time) %>%
+  summarise(count = sum(as.numeric(individualCount), na.rm = TRUE),
+            .groups = "drop")
+
+abund <- counts %>%
+  left_join(effort) %>%
   arrange(time) %>%
-  mutate(abundance = count / trapnights) %>% 
+  mutate(abundance = count / trapnights) %>%
   select(siteID, time, abundance) %>%
   ungroup()
 
 targets <- full_join(abund, richness)
-
 
 
 ##  Write out the targets
